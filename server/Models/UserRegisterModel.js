b4ed136c9eec356858a57ed50045229b1b9f5822
jidhinsyam <jidhin.botxswe2402@gmail.com>
2024-10-05 12:06:07 +0530

    commit main

---
+++ server/Models/UserRegisterModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const UserRegisterSchema = new Schema({
+    login_id:{type:Schema.Types.ObjectId,ref:'login_tb',reqired:true},
+     firstname:{type:String,required:true},
+     lastname:{type:String,required:true},
+     email:{type:String,required:true},
+     gender:{type:String,required:true},
+     dob:{type:String,required:true},
+     nationality:{type:String,required:true},
+     occupation:{type:String,required:true},
+     adress:{type:String,required:true},
+     contact:{type:String,required:true},
+     emergencycontact:{type:String,required:true},
+     role:{type:String,reqired:true},
+     
+ })
+ const UserRegisterModel = mongoose.model('Userregister_tb',UserRegisterSchema)
+ module.exports = UserRegisterModel
---
+++ server/Routes/UserRegisterRouter.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const registerRouter = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+
+registerRouter.post('/userreg',async (req, res)=>{
+    try{
+        const oldUser = await loginDB.findOne({email:req.body.email});
+        if(oldUser){
+            return res
+            .status(400)
+            .json({success:false,error:true,message:'user already exists'});
+
+        }
+        const oldPhone =await registerDB.findOne({contact:req.body.contact});
+        const hashedpassword = await bcrypt.hash(req.body.password,12);
+        if(oldPhone){
+            return res.status(400).json({
+                success:false,
+                error:true, 
+                message:'contact number already exists',
+
+            });
+        }
+        let log ={
+            email:req.body.email,
+            password:hashedpassword,
+            role:1,
+    
+        };
+        const result = await loginDB(log).save();
+        let reg={
+            login_id:result._id,
+            name:req.body.name,
+            adress:req.body.adress,
+            // email:req.body.email,
+            contact:req.body.contact,
+            age:req.body.age,
+        };
+        const result2 = await registerDB(reg).save();
+        if (result2) {
+         return   res.status(201).json({
+                success:true,
+                error:false,
+                massage:'registration completed',
+                data:result2,
+            });
+        }
+    }catch(error){
+       return res
+        .status(500)
+        .json({success:false,
+               error: true,
+               message:'somthing went wrong'
+            });
+        console.log(error);
+    }
+});
