b2bb46d8848d719e45848e448c5da5809deb0c46
jidhinsyam <jidhin.botxswe2402@gmail.com>
2024-10-05 17:07:37 +0530

    j

--- server/Routes/LoginRouter.js
+++ server/Routes/LoginRouter.js
@@ -2,60 +2,59 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const LoginModel = require('../Models/LoginModel');
-const UserRegisterModel= require('../Models/UserRegisterModel');
+const UserRegisterModel = require('../Models/UserRegisterModel');
 const loginRouter = express.Router();
 
 loginRouter.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    if (username && password) {
-      const oldUser = await LoginModel.findOne({ username });
-      if (!oldUser)
-        return res
-          .status(404)
-          .json({ success: false, error: true, message: "User doesn't Exist" });
-      const isPasswordCorrect = await bcrypt.compare(
-        password,
-        oldUser.password
-      );
-      if (!isPasswordCorrect)
-        return res
-          .status(400)
-          .json({ success: false, error: true, message: 'Incorrect password' });
+    const { username, password } = req.body;
+    try {
+        if (username && password) {
+            const oldUser = await LoginModel.findOne({ username });
+            if (!oldUser)
+                return res
+                    .status(404)
+                    .json({ success: false, error: true, message: "User doesn't Exist" });
+            const isPasswordCorrect = await bcrypt.compare(password,oldUser.password);
+            if (!isPasswordCorrect)
+                return res
+                    .status(400)
+                    .json({ success: false,
+                          error: true,
+                          message: 'Incorrect password' });
 
-      const token = jwt.sign(
-        {
-          loginId: oldUser._id,
-          userRole: oldUser.role,
-          userEmail: oldUser.email,
-        },
-        'secret_this_should_be_longer',
-        { expiresIn: '1h' }
-      );
-      
-      console.log('token', token);
-      const userdata  = await UserRegisterModel.findOne({login_id:oldUser._id})
-      return res.status(200).json({
-        success: true,
-        error: false,
-        token: token,
-        expiresIn: 3600,
-        loginId: oldUser._id,
-        userRole: oldUser.role,
-        userEmail: oldUser.email,
-        userId:userdata._id,
-        message:'login success'
-      });
-    } else {
-      return res.status(400).json({
-        success: false,
-        error: true,
-        message: 'All fields are required!',
-      });
+            const token = jwt.sign(
+                {
+                    loginId: oldUser._id,
+                    userRole: oldUser.role,
+                    userEmail: oldUser.email,
+                },
+                'secret_this_should_be_longer',
+                { expiresIn: '1h' }
+            );
+
+            console.log('token', token);
+            const userdata = await UserRegisterModel.findOne({ login_id: oldUser._id })
+            return res.status(200).json({
+                success: true,
+                error: false,
+                token: token,
+                expiresIn: 3600,
+                loginId: oldUser._id,
+                userRole: oldUser.role,
+                userUsername: oldUser.username,
+                userId: userdata._id,
+                message: 'login success'
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                error: true,
+                message: 'All fields are required!',
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' });
     }
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
 });
 
-module.exports=loginRouter;
+module.exports = loginRouter;
--- server/server.js
+++ server/server.js
@@ -1,49 +1,38 @@
 const express = require('express')
-const app= express()
+const app = express()
 const mongoose = require('mongoose')
- 
-const cors= require('cors')
+
+const cors = require('cors')
 const LoginRouter = require('./Routes/LoginRouter')
 const UserRegisterRouter = require('./Routes/UserRegisterRouter')
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 // setting headers
-app.use((req,res,next)=>{
-    res.setHeader('Access-Controll-Allow-Origin','*');
-    res.setHeader('Access-Controll-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Controll-Allow-Headers','Content-Type,Authorization');
+app.use((req, res, next) => {
+    res.setHeader('Access-Controll-Allow-Origin', '*');
+    res.setHeader('Access-Controll-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Controll-Allow-Headers', 'Content-Type,Authorization');
     next();
 });
 
-
 const mongoDBurl = "mongodb+srv://jidhinsyam:am123@cluster0.p6u8y.mongodb.net/"
-mongoose.connect(mongoDBurl,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect(mongoDBurl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
-    console.log("database connected"); 
+    console.log("database connected");
 }).catch((error) => {
     console.log(error);
 
 })
 
-app.use('/register',UserRegisterRouter),
-app.use('/login',LoginRouter),
+app.use('/register', UserRegisterRouter),
+    app.use('/login', LoginRouter),
 
 
 
 
-
-
-
- 
-
-
- 
- 
-
-
-app.listen(1000, () => { console.log("server started at http://localhost:1000"); })
+    app.listen(1000, () => { console.log("server started at http://localhost:1000"); })
