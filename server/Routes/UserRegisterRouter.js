5fa7ea1a1c4abbe7d28144606b56239b216a8d65
jidhinsyam <jidhin.botxswe2402@gmail.com>
2024-10-05 13:08:43 +0530

    Update UserRegisterRouter.js
    jm

--- server/Routes/UserRegisterRouter.js
+++ server/Routes/UserRegisterRouter.js
@@ -1,68 +1,70 @@
-const express = require('express')
+const express = require('express');
 const UserRegisterRouter = express.Router();
 const bcrypt = require('bcryptjs');
-const loginDB= require('../Models/LoginModel');
-const registerDB= require('../Models/UserRegisterModel');
+const loginDB = require('../Models/LoginModel');
+const registerDB = require('../Models/UserRegisterModel');
 
-
-
-UserRegisterRouter.post('/userregister',async (req, res)=>{
-    try{
-        const oldUser = await loginDB.findOne({username:req.body.username});
-        if(oldUser){
-            return res
-            .status(400)
-            .json({success:false,error:true,message:'user already exists'});
-
-        }
-        const oldPhone =await registerDB.findOne({contact:req.body.contact});
-        const hashedpassword = await bcrypt.hash(req.body.password,12);
-        if(oldPhone){
+UserRegisterRouter.post('/userregister', async (req, res) => {
+    try {
+        const oldUser = await loginDB.findOne({ username: req.body.username });
+        if (oldUser) {
             return res.status(400).json({
-                success:false,
-                error:true, 
-                message:'contact number already exists',
+                success: false,
+                error: true,
+                message: 'User already exists',
+            });
+        }
 
+        const oldPhone = await registerDB.findOne({ contact: req.body.contact });
+        if (oldPhone) {
+            return res.status(400).json({
+                success: false,
+                error: true,
+                message: 'Contact number already exists',
             });
         }
-        let log ={
-            username:req.body.username,
-            password:hashedpassword,
-            role:1,
-    
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        
+        let log = {
+            username: req.body.username,
+            password: hashedPassword,
+            role: 1,
         };
+
         const result = await loginDB(log).save();
-        let reg={
-            login_id:result._id,
-            firstname:req.body.firstname,
-            lastname:req.body.lastname,
-            gender:req.body.gender,
-            dob:req.body.dob,
-            nationality:req.body.nationality,
-            occupation:req.body.occupation,
-            address:req.body.address,
-            email:req.body.email,
-            contact:req.body.contact,
-            emergencycontact:req.body.emergencycontact,
-             
+
+        let reg = {
+            login_id: result._id,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            gender: req.body.gender,
+            dob: req.body.dob,
+            nationality: req.body.nationality,
+            occupation: req.body.occupation,
+            address: req.body.address,
+            email: req.body.email,
+            contact: req.body.contact,
+            emergencycontact: req.body.emergencycontact,
         };
+
         const result2 = await registerDB(reg).save();
+
         if (result2) {
-         return   res.status(201).json({
-                success:true,
-                error:false,
-                massage:'registration completed',
-                data:result2,
+            return res.status(201).json({
+                success: true,
+                error: false,
+                message: 'Registration completed',
+                data: result2,
             });
         }
-    }catch(error){
-       return res
-        .status(500)
-        .json({success:false,
-               error: true,
-               message:'somthing went wrong'
-            });
-       
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: true,
+            message: 'Something went wrong',
+        });
     }
 });
-module.exports=UserRegisterRouter;
+
+module.exports = UserRegisterRouter;
