22d80410e30b1aa1bac36cc52b55a4c700f11524
Libin-1998 <libinbotxBOTXSWe2403@gmail.com>
2024-10-05 12:01:57 +0530

    server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express=require('express')
+var mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const cors=require('cors')
+
+
+var app=express()
+
+dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('database connected');
+})
+.catch(()=>{
+    console.log('not connected');
+    
+})
+
+
+app.use(process.env.PORT,()=>{
+    console.log('running on',process.env.PORT);
+})
