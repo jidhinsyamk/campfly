341dd833368689fe4857fe855160b45772730465
jidhinsyam <jidhin.botxswe2402@gmail.com>
2024-10-05 15:02:09 +0530

    commit

--- server/Models/UserRegisterModel.js
+++ server/Models/UserRegisterModel.js
@@ -3,16 +3,16 @@ const Schema = mongoose.Schema;
 
 const UserRegisterSchema = new Schema({
     login_id:{type:Schema.Types.ObjectId,ref:'login_tb',reqired:true},
-     firstname:{type:String,required:true},
-     lastname:{type:String,required:true},
-     email:{type:String,required:true},
-     gender:{type:String,required:true},
-     dob:{type:String,required:true},
-     nationality:{type:String,required:true},
-     occupation:{type:String,required:true},
-     address:{type:String,required:true},
-     contact:{type:String,required:true},
-     emergencycontact:{type:String,required:true},
+     firstname:{type:String},
+     lastname:{type:String},
+     email:{type:String},
+     gender:{type:String},
+     dob:{type:String},
+     nationality:{type:String},
+     occupation:{type:String},
+     address:{type:String},
+     contact:{type:String},
+     emergencycontact:{type:String},
       
      
  })
--- server/server.js
+++ server/server.js
@@ -1,38 +1,49 @@
-var express=require('express')
-var mongoose=require('mongoose')
-const dotenv=require('dotenv')
-const cors=require('cors')
-const UserRegisterRouter = require('./Routes/UserRegisterRouter')
+const express = require('express')
+const app= express()
+const mongoose = require('mongoose')
+ 
+const cors= require('cors')
 const LoginRouter = require('./Routes/LoginRouter')
+const UserRegisterRouter = require('./Routes/UserRegisterRouter')
 
- 
- 
-
-
-var app=express()
-
-dotenv.config()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors())
 
+// setting headers
+app.use((req,res,next)=>{
+    res.setHeader('Access-Controll-Allow-Origin','*');
+    res.setHeader('Access-Controll-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Controll-Allow-Headers','Content-Type,Authorization');
+    next();
+});
+
+
+const mongoDBurl = "mongodb+srv://jidhinsyam:am123@cluster0.p6u8y.mongodb.net/"
+mongoose.connect(mongoDBurl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log("database connected"); 
+}).catch((error) => {
+    console.log(error);
+
+})
+
+app.use('/register',UserRegisterRouter),
+app.use('/login',LoginRouter),
+
+
+
+
+
+
+
  
 
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log('database connected');
-})
-.catch(()=>{
-    console.log('not connected');
-    
-})
  
-app.use('/api/register',UserRegisterRouter)
-app.use('/api/login',LoginRouter)
+ 
 
 
-app.use(process.env.PORT,()=>{
-    console.log('running on',process.env.PORT);
-})
-
+app.listen(1000, () => { console.log("server started at http://localhost:1000"); })
