069eda52fc3d2c90b30dfe0112f73e29972f027c
jidhinsyam <jidhin.botxswe2402@gmail.com>
2024-10-05 13:33:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const cors=require('cors')
  
+ 
 
 
 var app=express()
@@ -12,12 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Controll-Allow-Origin','*');
-    res.setHeader('Access-Controll-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Controll-Allow-Headers','Content-Type,Authorization');
-    next();
-});
+ 
 
 
 mongoose.connect(process.env.MONGO_URL)
